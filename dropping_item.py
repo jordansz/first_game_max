ab9f2ee6c610a48777903bfb43a9190db93fcf3d
jordansz <jszyma02@gmail.com>
2021-12-27 19:58:26 -0600

    minor change, saving for the night

---
+++ dropping_item.py
@@ -0,0 +1,13 @@
+class Dropping_item():
+    COOLDOWN = 30
+
+    def __init__(self,x, y , vell, on, id, imgs):
+        pass
+
+    def get_mask(self):
+        return self.mask 
+    
+    def set_dir(self, x):
+        self.dir = x
+    
+    def set_direction_photos(self, x, y):
